8eb621a6f81cf22c918ead6f412f18aaff862864
Phileader <phileader.com>
2019-08-03 15:50:32 -0400

    Config Material Icon

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,11 @@
-import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({
+export default new Vuetify({ 
   icons: {
     iconfont: 'mdi',
   },
